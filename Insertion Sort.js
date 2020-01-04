030e5630bbb69a2ff1b049e39026c3ef7c37ebac
GeorgeChryso <lsgechrys@gmail.com>
2020-01-04 19:40:57 +0200

    Commenting

--- Insertion Sort.js
+++ Insertion Sort.js
@@ -8,21 +8,21 @@ var InsertionSort = A => {
     let key = A[j]; // store the element to be moved
     let i = j - 1; // start examining all the elements left to it
 
+    // keep searching on the left until you find the first smaller element
     while (i > 0 && A[i] > key) {
-      //until you find a SMALLER element
       //Since you intend to insert your key at its rightful position that lies
       // on the left of j, you need to move all the other elements one side to the right
-      A[i + 1] = A[i]; //so at the same time you need to move all the bigger elements one side to the right
+      A[i + 1] = A[i]; //so that you can place your key 
       i--;
     }
-    // When you reach this step, you do have the ideal position i, where your key needs to be placed
-    A[i + 1] = key; // but A[i] is the first smaller element than your key, therefore you need to place your element after that
+    // When you reach this step, you do have the ideal position i that holds the first smaller element you came across
+    A[i + 1] = key; //  therefore you need to place your element after its first smaller element A[i]
   }
 
   return A;
 };
 
-// Complexity: O(n^2) worst case when the array is reverse sorted
+// Complexity: O(n^2) worst case when the array is reverse sorted, O(1) space
 // Best case O(n) when the array is already sorted
 
 console.log(
