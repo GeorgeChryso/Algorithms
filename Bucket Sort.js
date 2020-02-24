1d303c06f3bb12557c6f0614c7487b7ba98f8812
GeorgeChryso <lsgechrys@gmail.com>
2020-02-24 22:12:48 +0200

    commenting & complexity analysis

---
+++ Bucket Sort.js
@@ -0,0 +1,56 @@
+// Requirements:
+// Uniformly distributed input over a range
+// Can work in non integrers(floating)
+var InsertionSort = A => {
+    for (let j = 1; j < A.length; j++) {
+      let key = A[j]; // store the element to be moved
+      let i = j - 1; // start examining all the elements left to it
+  
+      // keep searching on the left until you find the first smaller element
+      while (i > 0 && A[i] > key) {
+        //Since you intend to insert your key at its rightful position that lies
+        // on the left of j, you need to move all the other elements one side to the right
+        A[i + 1] = A[i]; //so that you can place your key
+        i--;
+      }
+      // When you reach this step, you do have the ideal position i that holds the first smaller element you came across
+      A[i + 1] = key; //  therefore you need to place your element after its first smaller element A[i]
+    }
+  
+    return A;
+};
+
+let bucketSort=A=>{
+    //create buckets for each subcategory
+    // i.e. if you have decimals you can create 10 buckets
+    // 1 for each subdecimal
+    // However the criteria for different buckets relies on the coder
+    let buckets={}
+    for (let i = 0; i < 10; i++) {
+        buckets[i]=[]        
+    }
+
+    //place your array's items inside their respective bucket
+    for (let i = 0; i <A.length; i++) {
+        //according to their criteria
+        buckets[Math.floor(10*A[i])].push(A[i])        
+    }
+
+    //sort the buckets (usually using insertion sort
+    // for linear time ) 
+    for (const key in Object.keys(buckets)){
+        buckets[key]=InsertionSort(buckets[key])
+    }
+
+    //concatenate the buckets into the final array
+    return Object.values(buckets).reduce(
+        (acc,curr)=>acc.concat(curr),[]
+    )
+
+}
+//if all items are uniformly distributed over a range
+// then the algorithm takes O(n) time on average
+// and O(m) space, where m is the range my buckets memo was made according to
+console.log(bucketSort(
+    [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]
+))
--- Counting Sort.js
+++ Counting Sort.js
@@ -8,12 +8,16 @@ let countingSort=A=>{
 
     //1st. find the biggest element of A
     // ( the range of the algorithm)
-    let range=Math.max(...A)
+    let range=Math.max(...A) //considering the min is 0
+    //normally it would be
+    // range=Math.max(...A)-Math.min(...A)
     
     //2nd. Create the frequency array that stores occurences of its indices in A
     let frequency=[...Array(range+1)].map(d=>0)
     for (let i = 0; i < frequency.length; i++) {
-        frequency[A[i]]++        
+        frequency[A[i]]++  
+                //normally it would be frequency[A[i]-range]   
+                // to preserve the 0 index
     }
 
     //3d Fill the Running Sums
@@ -32,12 +36,17 @@ let countingSort=A=>{
     for (let i = 0; i < A.length; i++) {
         //notice the -1 due to 0 index
         sorted[frequency[A[i]]-1]=A[i]    
+            // again to preserve the index in the case of the min not being 0
+            // we would write 
+            // sorted[frequency[A[i]-range]-1]=A[i]
         frequency[A[i]]--
     }
 
     return sorted
 }
 
+
+  
 console.log(
     countingSort(
         [1,5,3,2,1,6]
