b8733253037c7a278b73ea117f8536d8df4068a6
GeorgeChryso <lsgechrys@gmail.com>
2020-03-02 22:58:08 +0200

    Final, minor changes

--- Data Structures/Binary Heap.js
+++ Data Structures/Binary Heap.js
@@ -6,13 +6,13 @@ class BinaryHeap{
         this.heap=[]
     }
 
-    hasParent=index=>index<1
+    hasParent=index=>index>=1
     getParent=(index)=>this.heap[Math.floor((index-1)/2)]
     
-    hasLeft=(index)=>2*index+1>this.heap.length-1
+    hasLeft=(index)=>2*index+1<=this.heap.length-1
     getLeftChild=(index)=>this.heap[2*index+1]
     
-    hasRight=index=>2*index+2>this.heap.length-1
+    hasRight=index=>2*index+2<=this.heap.length-1
     getRightChild=(index)=> this.heap[2*index+2]
     
 
@@ -22,9 +22,10 @@ class BinaryHeap{
     }
 
     bubbleUp(index){
-        while(this.hasParent(index)&&this.heap[index]<this.getParent(index)){
+        while(this.hasParent(index)&&(this.heap[index]<this.getParent(index))){
             //swap the two elements until the Invariant is reached
             [this.heap[index],this.heap[Math.floor((index-1)/2)]]= [this.heap[Math.floor((index-1)/2)],this.heap[index]]
+            index=Math.floor((index-1)/2)
         }
     }
 
@@ -41,11 +42,18 @@ class BinaryHeap{
 
             if(!this.hasRight(index)){
                 [this.heap[index],this.heap[index*2+1]]=[this.getLeftChild(index),this.heap[index]]
+                index=index*2+1
             }
             else{
 
-                if(this.getLeftChild(index)<=this.getRightChild(index)) [this.heap[index],this.heap[index*2+1]]=[this.getLeftChild(index),this.heap[index]]
-                else (this.getLeftChild(index)>this.getRightChild(index))[this.heap[index],this.heap[index*2+2]]=[this.getRightChild(index),this.heap[index]]
+                if(this.getLeftChild(index)<=this.getRightChild(index)){
+                  [this.heap[index],this.heap[index*2+1]]=[this.getLeftChild(index),this.heap[index]]
+                  index=index*2+1
+                }
+                else {
+                  [this.heap[index],this.heap[index*2+2]]=[this.getRightChild(index),this.heap[index]]
+                  index=index*2+2  
+                }
                 
             }
         }
@@ -56,7 +64,7 @@ class BinaryHeap{
 
 let hp=new BinaryHeap()
 
-let topush=[2,1,5,1,6,2]
+let topush=[2,1,32,1,0,2,5,0]
 
 topush.forEach(d=>{
     hp.push(d)
