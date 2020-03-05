6782d95281160c282118698a8162e53a49df9da3
GeorgeChryso <lsgechrys@gmail.com>
2020-03-05 03:00:12 +0200

    added a nlogn removal and lookup approach

--- Data Structures/Binary Heap.js
+++ Data Structures/Binary Heap.js
@@ -276,5 +276,143 @@ class MaxHeap {
   }
 
 
-  // I can implement a priority queue through Binary Search Trees aswell
-  
+//maxheap with a memo for O(nlogn) removals
+class maxBinaryHeap{
+    constructor(){
+        this.heap=[]
+        this.comparator=(a,b)=>b-a
+        this.valComparison=()=>null
+        this.store={} //this will store my heaps values as keys, and sets as values,sets that contain the key's position on my heap, it will provide nlogn loookup and removal
+    }
+
+    hasParent=index=>index>=1
+    getParent=(index)=>this.heap[Math.floor((index-1)/2)]
+    hasLeft=(index)=>2*index+1<=this.heap.length-1
+    getLeftChild=(index)=>this.heap[2*index+1]
+    hasRight=index=>2*index+2<=this.heap.length-1
+    getRightChild=(index)=> this.heap[2*index+2]
+    
+    length=()=>this.heap.length
+    peek=()=>this.heap[0]
+    push(element){
+        this.heap.push(element)
+        //this element is pushed on the rightmost node of the lowest level
+        // and needs  to be bubbled up accordingly
+        if(this.store[this.valComparison(element)]!==undefined)
+            this.store[this.valComparison(element)].add(this.heap.length-1)
+        else{
+            this.store[this.valComparison(element)]=new Set()
+            this.store[this.valComparison(element)].add(this.heap.length-1)
+        }
+        this.bubbleUp(this.heap.length-1)
+    }
+
+    bubbleUp(index){
+        //if there is a parent with a bigger priority, switch places with my index
+        while(
+            this.hasParent(index)&&
+            (this.comparator(this.heap[index],this.getParent(index))>=0)
+            ){
+            //swap the two elements until the Invariant is reached
+            this.swap(index,Math.floor((index-1)/2))
+            // and update the new index to be its parent's index, since u switched the items
+            index=Math.floor((index-1)/2)
+        }
+        
+    }
+
+    //get the highest(lowest) priority element
+    poll(){
+        if(this.length()==1){
+            this.store[this.valComparison(this.heap[this.heap.length-1])].delete(this.heap.length-1)
+            return this.heap.pop()
+        }
+
+        let result=this.heap[0]
+
+        this.swap(0,this.heap.length-1)
+        this.store[this.valComparison(this.heap[this.heap.length-1])].delete(this.heap.length-1)
+        this.heap.pop()
+        this.bubbleDown(0)
+        return result
+    }
+    
+    //after every poll, the new item on place 0 needs to be bubbled down to its correct position
+    bubbleDown(index){
+        if(this.length()<=1)return
+
+        while(this.hasLeft(index)&&( this.comparator(this.heap[index],this.getLeftChild(index))<0||(this.hasRight(index)&&this.comparator(this.heap[index],this.getRightChild(index))<0) )){
+
+            //if there is no right child, swap with the left
+            if(!this.hasRight(index)){
+                this.swap(index,index*2+1)
+                index=index*2+1
+            }
+            else{
+                // if the left child is less than or equal to the right child, choos the left
+   
+                if(this.comparator(this.getLeftChild(index),this.getRightChild(index))>=0){
+                    //and swap
+                  this.swap(index,index*2+1)
+                  index=index*2+1
+                }
+                // else choose the right child
+                else {
+                    //and swap
+                  this.swap(index,index*2+2)  
+                  index=index*2+2  
+                }
+                
+            }
+        }
+    }
+
+    heapify() {
+        if (this.length() < 2) return;
+        for (let i = 1; i < this.length(); i++) {
+          this.bubbleUp(i);
+        }
+      }
+
+
+      // I have to create a remove function that works in O(logn time)
+    remove=(height)=>{
+
+        if(!this.store[height].size){
+            return
+        }
+        else{
+            let index=-1
+          
+            for (const value of this.store[height].values()) {
+                index=value
+                break;
+            }
+            this.swap(index,this.heap.length-1) 
+            this.store[this.valComparison(this.heap[this.heap.length-1])].delete(this.heap.length-1)
+            this.heap.pop() //O(1)
+            this.bubbleDown(index) //logn
+        }
+
+    }
+
+
+    swap=(a,b)=>{
+        if(a===b)return
+        if(this.valComparison(this.heap[a])!=this.valComparison(this.heap[b])){
+            this.store[this.valComparison(this.heap[a])].delete(a)
+            this.store[this.valComparison(this.heap[a])].add(b)
+            this.store[this.valComparison(this.heap[b])].add(a)
+            this.store[this.valComparison(this.heap[b])].delete(b)
+        }
+        let temp=this.heap[b]
+        this.heap[b]=this.heap[a]
+        this.heap[a]=temp
+    }
+}
+
+
+  // I can implement a priority queue through Self balancing Binary Search Trees aswell
+
+
+
