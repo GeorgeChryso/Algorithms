a368aea5b298ce34c11bf899b51b1384e4a3d055
GeorgeChryso <lsgechrys@gmail.com>
2020-03-30 07:32:16 +0300

    modInverse thrugh Extended Euclidean

--- Maths/ModularInverse.js
+++ Maths/ModularInverse.js
@@ -26,4 +26,23 @@
  }
 
 
+ // A  number a has a modular inverse mod b  <=> gcd(a,b)=1
+ // then ax+by=1 ====> x is the modular inverse of a modb
+ // If a number a has a modular inverse modb , then I can find the triplet extended euclidean gives me
+ // x is the inverse of a modb
+// BUT, X CAN BE A NEGATIVE NUMBER, THAT'S WHY I HAVE TO  FIND THE EQUILEVANT POSITIVE NUMBER
+
+let modInverse=(a,b)=>{
+
+    let [g,x,y]=exEuc(a,b)
+    if(g!==1)return "Not possible"
+    
+    x=(x%b+b)%b //picks the positive x
+    return x
+}
+
 console.log(exEuc(10,12))
+console.log(modInverse(10,12))
+
+
+console.log(modInverse(11,13))
