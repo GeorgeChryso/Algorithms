59b344a789203f853b4fb869842c84f88d309bd4
GeorgeChryso <lsgechrys@gmail.com>
2020-09-30 19:59:46 +0300

    commenting

--- Maths/Geometry/Point Inside Polygon.js
+++ Maths/Geometry/Point Inside Polygon.js
@@ -3,7 +3,7 @@
 // given the vertices of a (convex) polygon, determine if a given point (X,Y) lies INSIDE the polygon
 
 
-// how to reconstruct a polygon given just its vertices? 
+// how to reconstruct a polygon given just its vertices? //aka find the convex hull
 
 
 //Intuition: 
