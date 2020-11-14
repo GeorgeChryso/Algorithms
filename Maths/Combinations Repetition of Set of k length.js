bdc2c616ca6ce908ec82f7f6f79ec63b8caa4f9c
GeorgeChryso <lsgechrys@gmail.com>
2020-11-14 19:12:27 +0200

    tool

---
+++ Maths/Combinations Repetition of Set of k length.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+// returns an array of all combinations out of the set of length k
+let printCombRep=(set,k)=>{
+    let result=[],n=set.length
+    let rec=(prefix)=>{
+        if(prefix.length==k)
+            return result.push([...prefix])//at this point prefix coitains the created array
+        for(let i=0;i<n;i++){
+            prefix.push(set[i])
+            rec(prefix)
+            prefix.pop()
+        }
+    }
+    rec([])
+    return result
+}
+
+console.log(
+    printCombRep(['A','B','C'],3)
+)
